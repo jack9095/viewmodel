9d9ab65ab6b8f39304044be8a60a46a7288f252e
fei.wang <fei.wang@molbase.com>
2019-03-25 16:41:31 +0800

    分析ViewModel源码

--- app/src/main/java/com/example/feiwang/viewmodelandlivedata/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/feiwang/viewmodelandlivedata/viewmodel/MainViewModel.java
@@ -2,6 +2,7 @@ package com.example.feiwang.viewmodelandlivedata.viewmodel;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.support.annotation.NonNull;
@@ -10,9 +11,13 @@ import android.support.annotation.NonNull;
  * Created by fei.wang on 2019/3/24.
  */
 public class MainViewModel extends AndroidViewModel {
-    MutableLiveData<String> mainLiveData = new MutableLiveData<>();
+    private final MutableLiveData<String> mainLiveData = new MutableLiveData<>();
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public LiveData<String> getLiveData() {
+        return mainLiveData;
     }
 }
